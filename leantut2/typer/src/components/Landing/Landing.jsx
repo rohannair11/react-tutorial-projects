f20f19f3c1a3d9158944c73013560f39d55bd946
rohan <rohan@pop-os.localdomain>
2022-03-14 19:03:17 +0530

    created basic landing page

--- leantut2/typer/src/components/Landing/Landing.jsx
+++ leantut2/typer/src/components/Landing/Landing.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 import './Landing.css'
 const Landing = () => {
   return (
-    <div>Landing</div>
+    <div className='landing-container'>
+        <div data-aos = "fade-right" className="landing-left">
+            <h1 className="landing-header">
+                simple typing test!     
+            </h1>    
+        </div>
+    </div>
   )
 }
 
